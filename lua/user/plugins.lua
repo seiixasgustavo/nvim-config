00fcc7256d54f88f956e5e041222e37db9a36e2e
Gustavo Seixas <gunabakbs@gmail.com>
2022-09-29 00:20:23 -0300

    feat: minor colorscheme additions

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -41,13 +41,18 @@ packer.init {
 -- Install your plugins here
 return packer.startup(function(use)
   -- My plugins here
-  use "wbthomason/packer.nvim" -- Have packer manage itself
-  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
-  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
-  
+  use "wbthomason/packer.nvim"  -- Have packer manage itself
+  use "nvim-lua/popup.nvim"     -- An implementation of the Popup API from vim in Neovim
+  use "nvim-lua/plenary.nvim"   -- Useful lua functions used ny lots of plugins
 
   -- Golang Plugins
   use "fatih/vim-go"    
+
+
+  -- Colorschemes
+  use "arzg/vim-colors-xcode"
+
+
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
