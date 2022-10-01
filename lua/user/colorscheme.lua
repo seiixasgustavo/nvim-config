87da084423f37c9d02ebf645e4e8327f6e7e0f95
Gustavo Seixas <gunabakbs@gmail.com>
2022-10-01 00:59:44 -0300

    feat: adds colorscheme configuration

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,7 +1,8 @@
-local colorscheme = "xcodedarkhc"
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
-  return
-end
+vim.cmd [[
+try
+  colorscheme xcodedarkhc
+catch /^Vim\%((\a\+)\)\=:E185/
+  colorscheme default
+  set background=dark
+endtry
+]]
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -40,10 +40,19 @@ packer.init {
 
 -- Install your plugins here
 return packer.startup(function(use)
+  
   -- My plugins here
   use "wbthomason/packer.nvim"  -- Have packer manage itself
   use "nvim-lua/popup.nvim"     -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim"   -- Useful lua functions used ny lots of plugins
+
+
+  -- cmp plugins
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
 
   -- Golang Plugins
   use "fatih/vim-go"    
@@ -51,8 +60,11 @@ return packer.startup(function(use)
 
   -- Colorschemes
   use "arzg/vim-colors-xcode"
+  use "folke/tokyonight.nvim"
 
-
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
